15153d1ff4a1084522e51287e2b2a44011ee2dc3
julian <julianhoffmann399@gmail.com>
2023-05-05 22:30:50 +0200

    v 2

--- MP3toM4R with FFmpeg (Windows Shell-Script)/MP3toM4R with FFmpeg (Windows Shell-Script).cpp
+++ MP3toM4R with FFmpeg (Windows Shell-Script)/MP3toM4R with FFmpeg (Windows Shell-Script).cpp
@@ -1,12 +1,20 @@
 // MP3toM4R with FFmpeg
-// Version 1
-
-// The paths still have to be set manually!
+// Version 2
 
 @echo off
-setlocal enabledelayedexpansion
-for%% a in("*.mp3") do (
-    ffmpeg - ss 0 - t 30 - i "%%a" - acodec aac - b:a 128k - f adts - map_metadata - 1 - vn "V:\Convert\bin\m4r_files_temp\%%~na.m4a"
-    ffmpeg - i "V:\Convert\bin\m4r_files_temp\%%~na.m4a" - acodec copy - t 29 - f adts - map_metadata - 1 - vn "V:\Convert\bin\m4r_files\%%~na.m4r"
-    del "V:\Convert\bin\m4r_files_temp\%%~na.m4a"
+set / p output_folder = Please enter the folder path where the converted files should be saved :
+
+if not exist "%output_folder%" (
+    echo The folder % output_folder % does not exist.The script will terminate.
+    pause
+    exit
     )
+
+    for%% a in("*.mp3") do (
+        ffmpeg - ss 0 - t 30 - i "%%a" - acodec aac - b:a 128k - f adts - map_metadata - 1 - vn "%output_folder%\%%~na.m4a"
+        ffmpeg - i "%output_folder%\%%~na.m4a" - acodec copy - t 29 - f adts - map_metadata - 1 - vn "%output_folder%\%%~na.m4r"
+        del "%output_folder%\%%~na.m4a"
+        )
+
+        echo Conversion completed.
+        pause
